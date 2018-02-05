0bfd0014557f747d34febe948d4693f9f7de4c06
Jerome <jerome.dury@flyingsheep.fr>
2018-02-05 10:37:18 +0100

    fix url pattern

--- tests/urls.py
+++ tests/urls.py
@@ -3,8 +3,7 @@ from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
 
-from admintab.urls import urlpatterns as admintab_urls
 
 urlpatterns = [
-    url(r'^', include(admintab_urls, namespace='admintab')),
+    url(r'^', include('admintab.urls', namespace='admintab')),
 ]
